208ba6900bc250a4afa2bfa368226a7fcd895a2a
Rullflex <libafer99@gmail.com>
2020-05-23 15:16:37 +0500

    uploaded all files

---
+++ assets/php_modules/variables.php
@@ -0,0 +1,22 @@
+<?php
+$base = "http://site/";
+//$base = "http://info.rully.ru/"
+//$base = "http://preview.rully.ru/";
+
+$main_page_link_adress = "http://site/";
+//$main_page_link_adress = "http://info.rully.ru/";
+//$main_page_link_adress = "http://preview.rully.ru/";
+
+//отключить отправку работы и доступ к регистрации/авторизации
+$sendwork_off = true;
+$auth_access_off = true;
+$calendar_off = true;
+
+$to_main_linktext = "На главную";
+//удалить когда ненадо будкет Лабы по классам
+$lab_po_classam_DEL = '<hr><a href="https://drive.google.com/drive/folders/1-hvBgrHFhUaH8o7F_48QOV4qyxEK8NrB">Лабораторные работы по классам</a>';
+
+
+
+
+?>
